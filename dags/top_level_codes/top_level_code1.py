e5cc38f259cc2129781eded293012b8e664e31bb
indraneeldongaonkar <indraneel.dongaonkar@astronomer.io>
2025-03-24 01:10:06 +0530

    Comment out unused code in top_level_code1.py for clarity

--- dags/top_level_codes/top_level_code1.py
+++ dags/top_level_codes/top_level_code1.py
@@ -1,22 +1,22 @@
-import pendulum
-from time import sleep
-from airflow import DAG
-from airflow.decorators import task
+# import pendulum
+# from time import sleep
+# from airflow import DAG
+# from airflow.decorators import task
 
-def expensive_api_call():
-    print("Hello from Airflow!")
-    # sleep(1000)
+# def expensive_api_call():
+#     print("Hello from Airflow!")
+#     # sleep(1000)
 
-my_expensive_response = expensive_api_call()
+# my_expensive_response = expensive_api_call()
 
-with DAG(
-    dag_id="top_level_code1",
-    schedule=None,
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
-    catchup=False,
-    tags=["example"],
-) as dag:
+# with DAG(
+#     dag_id="top_level_code1",
+#     schedule=None,
+#     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+#     catchup=False,
+#     tags=["example"],
+# ) as dag:
 
-    @task()
-    def print_expensive_api_call():
-        print(my_expensive_response)
+#     @task()
+#     def print_expensive_api_call():
+#         print(my_expensive_response)
